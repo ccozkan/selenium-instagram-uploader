c2a611da33e1d9b83694d13aac16607c9e77b610
Çağrı Özkan <cagrio.dev@gmail.com>
2019-11-22 17:53:35 +0300

    Add some notes on it

--- autokeyscript.py
+++ autokeyscript.py
@@ -1,16 +1,20 @@
-import time
-from stuff import image_path, image_name
+# !~~~ATTENTION~~~!
+# This file should be added to the scripts of autokey.
+# It can be done by using Autokey main window.
+# Save this script as image_select
+# This automation is suited for spacefm where ctrl-l activates the address bar and then searches image_name in that directory 
+# change according to your file manager
 
-# Enter script code
+image_path = '/path/to/your/image/folder/'
+image_name = '/image/name/no/need/to/file/extension'
+
 keyboard.send_keys('<ctrl>+l')
 keyboard.send_keys(image_path)
 
 keyboard.send_keys('<enter>')
 time.sleep(3)
 
-
-
-keyboard.send_keys('<ctrl>+f')
+#keyboard.send_keys('<ctrl>+f')
 keyboard.send_keys(image_name)
 keyboard.send_keys('<enter>')
 
