a5978293c29dfea4a0101e223b93b212553986c0
Çağrı Özkan <cagrio.dev@gmail.com>
2019-11-25 17:29:32 +0300

    Struggle on story upload

---
+++ upload_a_story.py
@@ -0,0 +1,75 @@
+import os
+import time
+from selenium import webdriver
+from credentials import username, password, description_file
+from selenium.webdriver.chrome.options import Options
+
+
+# FIREFOX
+#user_agent = "Mozilla/5.0 (iPhone; U; CPU iPhone OS 3_0 like Mac OS X; en-us) AppleWebKit/528.18 (KHTML, like Gecko) Version/4.0 Mobile/7A341 Safari/528.16"
+#profile = webdriver.FirefoxProfile() 
+#profile.set_preference("general.useragent.override", user_agent)
+#driver = webdriver.Firefox(profile)
+#driver.set_window_size(360,640)
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+mobile_emulation = {
+
+    "deviceMetrics": { "width": 360, "height": 640, "pixelRatio": 3.0 },
+    "userAgent": "Mozilla/5.0 (Linux; Android 4.2.1; en-us; Nexus 5 Build/JOP40D) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166 Mobile Safari/535.19" }
+
+chrome_options = Options()
+chrome_options.add_experimental_option("mobileEmulation", mobile_emulation)
+driver = webdriver.Chrome(chrome_options = chrome_options)
+
+# CHROME/CHROMIUM
+
+#mobile_emulation = {
+#    "deviceMetrics": { "width": 360, "height": 640, "pixelRatio": 3.0 },
+#    "userAgent": "Mozilla/5.0 (Linux; Android 4.2.1; en-us; Nexus 5 Build/JOP40D) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166 Mobile Safari/535.19" }
+#chrome_options = Options()
+#chrome_options.add_experimental_option("mobileEmulation", mobile_emulation)
+#driver = webdriver.Chrome(chrome_options = chrome_options)
+
+
+url = 'https://www.instagram.com/accounts/login/?source=auth_switcher'
+driver.get(url)
+time.sleep(10)
+
+
+field = driver.find_element_by_css_selector("input[type='text']")
+field.send_keys(username)
+field = driver.find_element_by_css_selector("input[type='password']")
+field.send_keys(password)
+time.sleep(2)
+button=driver.find_elements_by_xpath("//*[contains(text(), 'Log In')]")
+button[0].click()
+
+
+time.sleep(5)
+button=driver.find_elements_by_xpath("//*[contains(text(), 'Not Now')]")
+if len(button) > 0:
+    button[0].click()
+
+time.sleep(5)
+button=driver.find_elements_by_xpath("//*[contains(text(), 'Cancel')]")
+if len(button) > 0:
+    button[0].click()
+
+time.sleep(2)
+button = driver.find_elements_by_css_selector('[aria-label="New Story"]')
+button[0].click()
+time.sleep(5)
+os.system('autokey-run -s select_image')
+time.sleep(5)
+
+#driver.orientation = "LANDSCAPE"
+
+#button = driver.find_elements_by_css_selector('[aria-label="Add to your story"]')
+#time.sleep(50)
+
+
+
+#driver.quit()
