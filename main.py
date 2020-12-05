c6394f99d3c826821507f14b783e72cecef2055f
Çağrı Özkan <cagrio.dev@gmail.com>
2020-12-05 22:11:29 +0300

    Prettify

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from selenium import webdriver
 from credentials import username, password, description_file
 from selenium.webdriver.common import action_chains
 
-
 # FIREFOX
 user_agent = "Mozilla/5.0 (iPhone; U; CPU iPhone OS 3_0 like Mac OS X; en-us) AppleWebKit/528.18 (KHTML, like Gecko) Version/4.0 Mobile/7A341 Safari/528.16"
 profile = webdriver.FirefoxProfile() 
@@ -12,9 +11,7 @@ profile.set_preference("general.useragent.override", user_agent)
 driver = webdriver.Firefox(profile)
 driver.set_window_size(360,640)
 
-
 # CHROME/CHROMIUM
-
 #from selenium.webdriver.chrome.options import Options
 #mobile_emulation = {
 #    "deviceMetrics": { "width": 360, "height": 640, "pixelRatio": 3.0 },
@@ -23,11 +20,9 @@ driver.set_window_size(360,640)
 #chrome_options.add_experimental_option("mobileEmulation", mobile_emulation)
 #driver = webdriver.Chrome(chrome_options = chrome_options)
 
-
 url = 'https://www.instagram.com/accounts/login/?source=auth_switcher'
 driver.get(url)
 time.sleep(10)
-
 
 field = driver.find_element_by_css_selector("input[type='text']")
 field.send_keys(username)
@@ -36,7 +31,6 @@ field.send_keys(password)
 time.sleep(2)
 button=driver.find_elements_by_xpath("//*[contains(text(), 'Log In')]")
 button[0].click()
-
 
 time.sleep(5)
 button=driver.find_elements_by_xpath("//*[contains(text(), 'Not Now')]")
@@ -51,10 +45,8 @@ if len(button) > 0:
 time.sleep(2)
 button = driver.find_elements_by_css_selector('[aria-label="New Post"]')
 button[0].click()
-#button1 = driver.find_elements_by_css_selector('[role="menuitem"]')
-#button1[-1].click()
 
-# os.system('autokey-run -s select_image')
+os.system('autokey-run -s select_image')
 
 time.sleep(10)
 button=driver.find_elements_by_xpath("//*[contains(text(), 'Expand')]")
@@ -75,12 +67,11 @@ with open(description_file, 'r') as file:
 field.send_keys(description)
 
 time.sleep(15)
-button=driver.find_elements_by_xpath("//*[contains(text(), 'Share')]")
-temp = button[1]
+button = driver.find_elements_by_xpath("//*[contains(text(), 'Share')]")[1]
 
-driver.execute_script("arguments[0].scrollIntoView();", temp);
+driver.execute_script("arguments[0].scrollIntoView();", button);
 action = action_chains.ActionChains(driver)
-action.move_to_element(temp)
+action.move_to_element(button)
 action.click()
 action.perform()
 
