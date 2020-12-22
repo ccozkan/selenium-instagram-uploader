2f19f522d9f6a16a61ba189773175aa9394df102
Çağrı Özkan <cagrio.dev@gmail.com>
2020-12-23 00:20:33 +0300

    Fix typo

--- autokeyscript.py
+++ autokeyscript.py
@@ -1,14 +1,14 @@
 # !~~~ATTENTION~~~!
 # This file should be added to the scripts of autokey.
 # It can be done by using Autokey main window.
-# Save this script as image_select
+# Save this script as select_image
 # This automation is suited for spacefm where ctrl-l activates the address bar and then searches image_name in that directory 
 # change according to your file manager
 
 import time
 
 image_path = '/path/to/your/image/folder/'
-image_name = '/image/name/no/need/to/file/extension'
+image_name = 'image_name_no_need_to_file_extension'
 
 keyboard.send_keys('<ctrl>+l')
 keyboard.send_keys(image_path)
